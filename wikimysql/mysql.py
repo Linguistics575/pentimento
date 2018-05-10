fb9314e9a6512639c7593d9613ab2e9bce3850c3
Eslam Elsawy <eslam@uw.edu>
2018-05-09 21:14:24 -0700

    Updating name variations output

--- wikimysql/mysql.py
+++ wikimysql/mysql.py
@@ -7,55 +7,83 @@ def main():
     input_file_name = "input.txt"
     conn = MySQLdb.connect(host="localhost",
                            user="root",
-                           passwd="**",
+                           passwd="****",
                            db="wikipedia")
 
     previous_output_root = ET.parse(output_file_name).getroot()
     previous_output_dic = {}
-    for title in previous_output_root:
-        previous_output_dic[title.text.strip()] = title
+    for previous_location in previous_output_root:
+        previous_output_dic[previous_location.text.strip()] = previous_location
 
     with open(input_file_name) as f:
         locations = f.readlines()
 
     output_root = ET.Element("Variations")
 
-    for location in locations:
-        if not location.strip() or location.startswith("--"):
+    for location_list_str in locations:
+        if not location_list_str.strip() or location_list_str.startswith("--"):
             continue
 
-        location = location.strip()
+        location_list = location_list_str.strip().split(";")
 
-        if location in previous_output_dic:
-            print("Cache Hit:" + location)
-            output_root.append(previous_output_dic[location])
-            continue
+        wiki_page_title = location_list[0]
+        manual_variations = location_list[1:] if len(location_list) > 1 else []
 
-        print("Query:"+location)
+        # getting the modern name
+        modren_name = ""
+        for manual_variation in manual_variations:
+            if manual_variation.startswith("\""):
+                modren_name = manual_variation
+                break
+        if modren_name:
+            manual_variations.remove(modren_name)
+            modren_name = modren_name[1:-1] # remove quotes
+            manual_variations.append(modren_name)
+
+
+        # getting wiki variations
+        wiki_variations = []
+        if wiki_page_title in previous_output_dic:
+            print("Cache Hit:" + wiki_page_title)
+            previous_element = previous_output_dic[wiki_page_title]
+            for wiki_variation_element in previous_element.findall('wiki-variation'):
+                wiki_variations.append(wiki_variation_element.text)
+        else:
+            # query wiki db for variations
+            print("Query:"+ wiki_page_title)
+            cursor = conn.cursor()
+            cursor.execute("SELECT  p.page_title, r.rd_title  "
+                           "FROM redirect as r "
+                           "JOIN page as p "
+                           "ON p.page_id = r.rd_from "
+                           "where r.rd_title = '" + wiki_page_title.strip() + "'")
+            rows = cursor.fetchall()
+            for row in rows:
+                variation = row[0].decode("utf-8")
+                wiki_variations.append(variation)
+                print("%s" % variation)
+            print("\nNumber of rows returned: %d" % cursor.rowcount)
+            print("-------")
+            cursor.close()
 
         # adding the location name as title.txt and one of the variations
-        location_element = ET.SubElement(output_root, "title")
-        location_element.text = location
+        location_element = ET.SubElement(output_root, "location")
+        location_element.text = wiki_page_title
 
-        variation_element = ET.SubElement(location_element, "variation")
-        variation_element.text = location
+        variation_element = ET.SubElement(location_element, "wiki-page")
+        variation_element.text = wiki_page_title
 
-        # query wiki db for variations
-        cursor = conn.cursor()
-        cursor.execute("SELECT  p.page_title, r.rd_title  "
-                       "FROM redirect as r "
-                       "JOIN page as p "
-                       "ON p.page_id = r.rd_from "
-                       "where r.rd_title = '" + location.strip() + "'")
-        rows = cursor.fetchall()
-        for row in rows:
-            variation = row[0].decode("utf-8")
-            variation_element = ET.SubElement(location_element, "variation")
-            variation_element.text = variation
-            print("%s" % variation)
-        print("\nNumber of rows returned: %d" % cursor.rowcount)
-        print("-------")
-        cursor.close()
+        if modren_name:
+            variation_element = ET.SubElement(location_element, "modern-name")
+            variation_element.text = modren_name
+
+        for manual_variation in manual_variations:
+            variation_element = ET.SubElement(location_element, "manual-variation")
+            variation_element.text = manual_variation
+
+        for wiki_variation in wiki_variations:
+            variation_element = ET.SubElement(location_element, "wiki-variation")
+            variation_element.text = wiki_variation
 
     conn.close()
 
