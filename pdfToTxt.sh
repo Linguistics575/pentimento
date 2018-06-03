2af2a95d10cabb625686fed58c07756f7ae1e532
tguinard <tguinard@gmail.com>
2018-06-03 15:38:54 -0700

    better to remove the tiffs

--- pdfToTxt.sh
+++ pdfToTxt.sh
@@ -17,4 +17,6 @@ do
     convert -density 300 "$2/pdfs/$i.pdf" "$2/pngs/$i.png"
     python3 ocr/do_ocr.py "$2/tiffs/$i-500.tiff" > "$2/raw_ocr/500/$i.txt"
     python3 ocr/do_ocr.py "$2/tiffs/$i-200.tiff" > "$2/raw_ocr/200/$i.txt"
+    rm "$2/tiffs/$i-200.tiff"
+    rm "$2/tiffs/$i-500.tiff"
 done
